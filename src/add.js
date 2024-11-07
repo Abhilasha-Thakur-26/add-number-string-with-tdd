48ffc7ff6249ab0254a07e59335ef504867edc99
Abhilasha Thakur <Abhilasha.Thakur@navigaglobal.com>
2024-11-07 19:49:17 +0530

    added excpetion when negative number as an entry are provided

--- src/add.js
+++ src/add.js
@@ -1,5 +1,9 @@
 function add(numbers) {
-    const numArray = numbers.split(',').map(num => Number(num.trim())).filter(num => !isNaN(num));
-    return numArray.reduce((sum, num) => sum + num, 0);
+    const numArray = numbers.split(',').map(num => Number(num.trim())).filter(num => !isNaN(num))
+    const negativeNumbers = numArray.filter(num => num < 0)
+    if (negativeNumbers.length > 0) {
+        throw new Error(`Negative numbers not allowed: ${negativeNumbers.join(', ')}`)
+    }
+    return numArray.reduce((sum, num) => sum + num, 0)
 }
-module.exports = add;
+module.exports = add
--- src/add.test.js
+++ src/add.test.js
@@ -1,11 +1,11 @@
 const add = require('./add.js')
 
 test('adds string with a single number', () => {
-    expect(add("1")).toBe(1);
+    expect(add("1")).toBe(1)
 });
 
 test('adds string of numbers separated by commas', () => {
-    expect(add("5,10,16")).toBe(31);
+    expect(add("5,10,16")).toBe(31)
 });
 
 test('handles string of numbers having extra spcaces ahead of numbers',() =>{
@@ -19,6 +19,14 @@ test('handles string of numbers having extra spcaces at the end of numbers',() =
 /*  Since the task description didn't mention
     about entries which are not numbers I am taking it as edge 
     case and for now I am filtering out entries which are NaN */
+
 test('handles entries which are not numbers ',()=>{
     expect(add("5, 1 0, 16")).toBe(21)
+    expect(add("5, , 16")).toBe(21)
+    expect(add("")).toBe(0)
+})
+
+test('throws exception for negative numbers ',()=>{
+    expect(() => { add("20, 4, -10, 0") }).toThrow('Negative numbers not allowed: -10')
+    expect(() => { add("9, 7, -2, -4")}).toThrow('Negative numbers not allowed: -2, -4')
 })
