6bf645c89ed3560787ae6842e27769e12c3f9989
Abhilasha Thakur <Abhilasha.Thakur@navigaglobal.com>
2024-11-07 19:26:39 +0530

    handled leading and trailling spaces in numbers

--- src/add.js
+++ src/add.js
@@ -1,5 +1,5 @@
 function add(numbers) {
-    const numArray = numbers.split(',').map(Number);
+    const numArray = numbers.split(',').map(num => Number(num.trim()));
     return numArray.reduce((sum, num) => sum + num, 0);
 }
 module.exports = add;
--- src/add.test.js
+++ src/add.test.js
@@ -7,4 +7,11 @@ test('adds string with a single number', () => {
 test('adds string of numbers separated by commas', () => {
     expect(add("5,10,16")).toBe(31);
 });
-  
+
+test('handles string of numbers having extra spcaces ahead of numbers',() =>{
+    expect(add("5, 10, 16")).toBe(31)
+})
+
+test('handles string of numbers having extra spcaces at the end of numbers',() =>{
+    expect(add("5, 10 , 16 ")).toBe(31)
+})
