dbdcb1a6a2c1027f5e36e1141b4d0fa85a4b0cca
Abhilasha Thakur <Abhilasha.Thakur@navigaglobal.com>
2024-11-07 20:55:22 +0530

    handles negative and NaN entries

--- src/add.js
+++ src/add.js
@@ -1,9 +1,18 @@
 function add(numbers) {
-    const numArray = numbers.split(',').map(num => Number(num.trim())).filter(num => !isNaN(num))
-    const negativeNumbers = numArray.filter(num => num < 0)
+    const numArray = numbers.split(',')
+    .map(num => {
+        const parsedNum = Number(num.trim())
+        return {num, parsedNum}
+    })
+    const validNumbers = numArray.map(item => item.parsedNum).filter(num => !isNaN(num));
+    const negativeNumbers = validNumbers.filter(num => num < 0)
     if (negativeNumbers.length > 0) {
         throw new Error(`Negative numbers not allowed: ${negativeNumbers.join(', ')}`)
     }
-    return numArray.reduce((sum, num) => sum + num, 0)
+    const invalidEntries = numArray.filter(entry => isNaN(entry.parsedNum)).map(entry => entry.num.trim())
+    if(invalidEntries.length > 0){
+        throw new Error(`Invalid inputs: ${invalidEntries.join(', ')}`)
+    }
+    return validNumbers.reduce((sum, num) => sum + num, 0)
 }
 module.exports = add
--- src/add.test.js
+++ src/add.test.js
@@ -10,6 +10,7 @@ test('adds string of numbers separated by commas', () => {
 
 test('handles string of numbers having extra spcaces ahead of numbers',() =>{
     expect(add("5, 10, 16")).toBe(31)
+    expect(add("")).toBe(0)
 })
 
 test('handles string of numbers having extra spcaces at the end of numbers',() =>{
@@ -18,15 +19,23 @@ test('handles string of numbers having extra spcaces at the end of numbers',() =
 
 /*  Since the task description didn't mention
     about entries which are not numbers I am taking it as edge 
-    case and for now I am filtering out entries which are NaN */
+    case and for now I am throwing exceptions for entries which are NaN */
 
 test('handles entries which are not numbers ',()=>{
-    expect(add("5, 1 0, 16")).toBe(21)
-    expect(add("5, , 16")).toBe(21)
-    expect(add("")).toBe(0)
+    expect(() => {add("5, 1 0, 16")}).toThrow('Invalid inputs: 1 0')
+    expect(() => {add("5,abc, 1n")}).toThrow('Invalid inputs: abc, 1n')
+    expect(() => {add("6, 5,' '")}).toThrow('Invalid inputs: \' \'')
 })
 
 test('throws exception for negative numbers ',()=>{
     expect(() => { add("20, 4, -10, 0") }).toThrow('Negative numbers not allowed: -10')
     expect(() => { add("9, 7, -2, -4")}).toThrow('Negative numbers not allowed: -2, -4')
+})
+
+//for both NaN and negative numbers 
+test('throws exception for negative numbers ',()=>{
+    expect(() => { add("20, 4, -10, 0, abc") }).toThrow('Negative numbers not allowed: -10')
+    //even if we change the position of both
+    expect(() => { add("20, abc, -10, 0, 4") }).toThrow('Negative numbers not allowed: -10')
+
 })
